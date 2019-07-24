633e3fec046fb1692843c2f68b7ad8175ae8b8ba
Anton Trollbäck <anton@trollback.se>
2019-07-24 17:56:48 +0200

    Add wip versions of all needed slices

--- components/slices/index.js
+++ components/slices/index.js
@@ -1,7 +1,7 @@
 var html = require('choo/html')
 var asElement = require('prismic-element')
 var embed = require('../embed')
-var { resolve, srcset, src, memo } = require('../base')
+var { resolve, srcset, src } = require('../base')
 
 module.exports = slices
 
@@ -11,15 +11,42 @@ function slices (slice, index, list, onclick) {
       if (!slice.primary.text.length) return null
       return html`
         <div class="u-container">
+          <div class="u-padded">
             ${asElement(slice.primary.text, resolve)}
+          </div>
+        </div>
+      `
+    }
+    case 'hero': {
+      if (!slice.primary.text.length || !slice.primary.image.url) return null
+      let image = slice.primary.image
+      let attrs = Object.assign({ alt: image.alt || '' }, image.dimensions)
+      if (!/\.(svg|gif?)$/.test(image.url)) {
+        attrs.sizes = '100vw'
+        attrs.srcset = srcset(
+          image.url,
+          [640, 750, 1125, 1440, [2880, 'q_50'], [3840, 'q_50']]
+        )
+      }
+      return html`
+        <div class="u-container">
+          <div class="u-padded">
+            <div class="Hero">
+              <div class="Hero-body">
+                ${asElement(slice.primary.text, resolve)}
+              </div>
+              <figure class="Hero-figure">
+                <img ${attrs} src="${src(slice.primary.image.url, 800)}">
+              </figure>
+            </div>
+          </div>
         </div>
       `
     }
     case 'image': {
       if (!slice.primary.image.url) return null
-      var caption = slice.primary.image.alt
-      var image = slice.primary.image
-      var attrs = Object.assign({ alt: image.alt || '' }, image.dimensions)
+      let image = slice.primary.image
+      let attrs = Object.assign({ alt: image.alt || '' }, image.dimensions)
       if (!/\.(svg|gif?)$/.test(image.url)) {
         attrs.sizes = '100vw'
         attrs.srcset = srcset(
@@ -31,30 +58,38 @@ function slices (slice, index, list, onclick) {
       return html`
         <figure>
           <img ${attrs} src="${src(slice.primary.image.url, 800)}">
-          <div class="u-container">
-            ${caption ? html`<figcaption class="Text-caption">${caption}</figcaption>` : null}
-          </div>
         </figure>
       `
     }
     case 'line': {
-      let prev = list[index - 1]
-      let next = list[index + 1]
-      let narrow = (next && next.slice_type === 'text') && (!prev || prev.slice_type === 'text')
       return html`
         <div class="u-container">
-          <hr class="${narrow ? 'u-medium' : ''}">
+          <hr class="u-padded">
         </div>
       `
     }
     case 'video': {
-      if (slice.primary.video.type !== 'video') return null
-      let children = video(slice.primary.video)
-      if (!children) return null
+      if (!slice.primary.image.url) return null
+      let image = slice.primary.image
+      let attrs = Object.assign({ alt: image.alt || '' }, image.dimensions)
+      if (!/\.(svg|gif?)$/.test(image.url)) {
+        attrs.sizes = '100vw'
+        attrs.srcset = srcset(
+          image.url,
+          [640, 750, 1125, 1440, [2880, 'q_50'], [3840, 'q_50']]
+        )
+      }
 
       return html`
-        <div class="u-md-container">
-          ${children}
+        <div class="u-lg-container">
+          <div class="u-video">
+            <img ${attrs} src="${src(slice.primary.image.url, 800)}">
+            <video preload="metadata" controls disablePictureInPicture playsinline muted width="960" height="540" class="u-video" poster="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==">
+              <source src="https://res.cloudinary.com/dykmd8idd/video/upload/s--9JroGugU--/vc_h265,w_960/v1563978786/trimmed_dhpmkk.mp4" type="video/mp4; codecs=hvc1">
+              <source src="https://res.cloudinary.com/dykmd8idd/video/upload/s--9JroGugU--/vc_vp9,w_960/v1563978786/trimmed_dhpmkk.webm" type="video/webm; codecs=vp9">
+              <source src="https://res.cloudinary.com/dykmd8idd/video/upload/s--9JroGugU--/vc_auto,w_960/v1563978786/trimmed_dhpmkk.mp4" type="video/mp4"> 
+            </video>
+          </div>
         </div>
       `
     }
