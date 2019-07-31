db8adbc550339e12ceeef645842a40fba079d5ce
Anton Trollbäck <anton@trollback.se>
2019-07-31 15:21:16 +0200

    Add symbol to header

--- components/header/index.js
+++ components/header/index.js
@@ -5,11 +5,21 @@ module.exports = header
 function header (props) {
   return html`
     <header class="Header">
+      <svg class="Header-repeat" preserveAspectRatio="none" style="display: none;" role="presentation" viewBox="0 0 371 88">
+        <g fill="none" fill-rule="evenodd">
+          <path vector-effect="non-scaling-stroke" stroke="currentColor" stroke-linecap="square" stroke-linejoin="round" stroke-width="10" d="M0 44h371"/>
+        </g>
+      </svg>
       <a href="/" class="Header-start">
-        <svg role="presentation" class="Header-icon" viewBox="0 0 184 50">
-          <path d="M0 25h58V3l64 44V25h62" stroke="#000" stroke-width="5" fill="none" fill-rule="evenodd" stroke-linejoin="round"/>
+        <svg class="Header-symbol" role="presentation" viewBox="0 0 365 88">
+          <g fill="none" fill-rule="evenodd">
+            <path fill="#fff" d="M.5 0h363.5v88H.5z"/>
+            <path vector-effect="non-scaling-stroke" stroke="currentColor" stroke-linecap="square" stroke-linejoin="round" stroke-width="10" d="M5 44h118V5l121 78V44h116"/>
+          </g>
         </svg>
-        Ngaren
+        <div class="Header-logotype">
+          <span>N</span><span>g</span><span>a</span><span>r</span><span>e</span><span>n</span>
+        </div>
       </a>
       <nav class="Header-wrap">
         <ul class="Header-nav">
